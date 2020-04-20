86925d2dd853c85908189fdb36ecebab579ee3f9
Gabriel Porto <gr_porto@hotmail.com>
2020-04-20 15:53:42 -0300

    Seller details and eager loading

--- WebApplication/Service/SellerService.cs
+++ WebApplication/Service/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using WebApplication.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication.Service
 {
@@ -17,7 +18,7 @@ namespace WebApplication.Service
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Insert(Seller obj)
