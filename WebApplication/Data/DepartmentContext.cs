95d988d79dac76c5f227d85273938c6023488c68
Gabriel Porto <gr_porto@hotmail.com>
2020-04-20 11:46:49 -0300

    Theme update for Semantic

---
+++ WebApplication/Data/DepartmentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class DepartmentContext : DbContext
+    {
+        public DepartmentContext (DbContextOptions<DepartmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
