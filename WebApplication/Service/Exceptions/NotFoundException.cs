8c1cd6fc083d75cd33a0aed9aab7d56325e984f0
Gabriel Porto <gr_porto@hotmail.com>
2020-04-20 16:23:42 -0300

    Update Seller and custom service exception

---
+++ WebApplication/Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApplication.Service.Exceptions
+{
+    public class NotFoundException: ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
--- WebApplication/Service/SellerService.cs
+++ WebApplication/Service/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using WebApplication.Models;
 using Microsoft.EntityFrameworkCore;
+using WebApplication.Service.Exceptions;
 
 namespace WebApplication.Service
 {
@@ -26,6 +27,25 @@ namespace WebApplication.Service
             _context.Add(obj);
 
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
 
         public void Remove(int id)
